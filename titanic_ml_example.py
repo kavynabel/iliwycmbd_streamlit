2d347586e4c9fa6231d0badbc242c9916bb8bc81
Soren Laney <90982304+sorenlaney@users.noreply.github.com>
2022-11-04 14:23:18 -0600

    add df to file

--- titanic_ml_example.py
+++ titanic_ml_example.py
@@ -1,8 +1,9 @@
 
 #%%
-
 import pandas as pd
 
 
  #%%
-  main_df =         pd.read_csv('https://github.com/byuibigdata/iliwycmbd_streamlit/blob/main/titanic.csv')
+ main_df =         pd.read_csv('https://github.com/byuibigdata/iliwycmbd_streamlit/blob/main/titanic.csv')
+ 
+ 
