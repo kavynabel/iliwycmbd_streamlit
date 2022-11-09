597fa36e9ad2ec2145eb86bb9514c43e8864bd53
Trey L <90742156+UptoCode99@users.noreply.github.com>
2022-11-09 14:37:36 -0700

    user friendly
    I updated it so the user can see their progress as they move on

--- iris_app.py
+++ iris_app.py
@@ -8,8 +8,7 @@ iris = pd.read_csv('https://raw.githubusercontent.com/byuibigdata/iliwycmbd_stre
 iris = iris.rename(columns={'sepal.length': 'sepallength', 'sepal.width': 'sepalwidth', 'petal.length': 'petallength', 'petal.width': 'petalwidth'})
 
 # For the ml model dedicate which columns will be your feature under 'X' and our target under 'Y'
-X
-Y
+
 
 # Add a title to your app, shortcuts included in the links given under Header.
 
@@ -21,18 +20,18 @@ Y
 
 
 # bellow is the code to add sliders and to connect them to a dataset which we will use for our ml model. Add a slider for sepal length 
-def user_input_features():
-    sepalwidth = st.sidebar.slider('Sepal Width', 0.1,8.1,4.0, step=.1)
-    petallength = st.sidebar.slider('Petal Length',0.1,2.1,1.1, step=.1)
-    petalwidth = st.sidebar.slider('Petal Width', 0.1,2.1,1.1, step=.1)
-    data ={ 'sepalwidth':sepalwidth,
-            'petalwidth':petalwidth,
-            'petallength':petallength
-    }
-    features = pd.DataFrame(data, index=[0])
-    return features
+#def user_input_features():
+ #   sepalwidth = st.sidebar.slider('Sepal Width', 0.1,8.1,4.0, step=.1)
+  #  petallength = st.sidebar.slider('Petal Length',0.1,2.1,1.1, step=.1)
+   # petalwidth = st.sidebar.slider('Petal Width', 0.1,2.1,1.1, step=.1)
+    #data ={ 'sepalwidth':sepalwidth,
+     #       'petalwidth':petalwidth,
+      #      'petallength':petallength
+    #}
+    #features = pd.DataFrame(data, index=[0])
+    #return features
 
-df = user_input_features()
+#df = user_input_features()
 
 
 # load pickel with our ml model
