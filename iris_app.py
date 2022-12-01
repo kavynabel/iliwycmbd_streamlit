01600f4672a64fc66284774312debf9d276fdf5c
Emma Holt <81181898+emmaholt833@users.noreply.github.com>
2022-12-01 11:58:14 -0700

    Update iris_app.py

--- iris_app.py
+++ iris_app.py
@@ -1,15 +1,15 @@
 # Import packages
 
 
-# Load Data rename columns 
+# Load data & rename columns 
 
 iris = pd.read_csv('https://raw.githubusercontent.com/byuibigdata/iliwycmbd_streamlit/main/iris.csv')
 
 iris = iris.rename(columns={'sepal.length': 'sepallength', 'sepal.width': 'sepalwidth', 'petal.length': 'petallength', 'petal.width': 'petalwidth'})
 
 # For the ml model dedicate which columns will be your feature under 'X' and our target under 'Y'
-X
-Y
+X = 
+Y = 
 
 # Add a title to your app, shortcuts included in the links given under Header.
 
@@ -20,7 +20,8 @@ Y
 # add a title for your sidebar
 
 
-# bellow is the code to add sliders and to connect them to a dataset which we will use for our ml model. Add a slider for sepal length 
+# Below is the code to add sliders and to connect them to a dataset which we will use for our ml model
+# Add a slider for sepal length 
 def user_input_features():
     sepalwidth = st.sidebar.slider('Sepal Width', 0.1,8.1,4.0, step=.1)
     petallength = st.sidebar.slider('Petal Length',0.1,2.1,1.1, step=.1)
@@ -35,15 +36,17 @@ def user_input_features():
 df = user_input_features()
 
 
-# load pickel with our ml model
+# Load the pickled ML model
 
 load_clf = pickle.load(open('load pickle here', 'rb'))
 
-# load prediction and prediction probability 
+# Load prediction & prediction probability 
 prediction = load_clf.predict(df)
 prediction_proba = load_clf.predict_proba(df)
 
-# display the data that the user inputed and display the prediction
+# Display the data that the user inputed and display the prediction
 
 
-# add any charts, visuals or widgets that you would like 
+# Add any charts, visuals or widgets that you would like 
+
+
